399eb3703b8ecf910e663600fd2037b3734ecd59
Volodymyr Stashenko <volodyasilver@gmail.com>
2026-03-24 22:06:31 +0200

    Refactor movie_session method parameters for clarity

--- app/people/hall.py
+++ app/people/hall.py
@@ -1,8 +1,16 @@
+from typing import Any
+
+
 class CinemaHall:
     def __init__(self, hall_number: int) -> None:
         self.hall_number = hall_number
 
-    def movie_session(self, movie_name: str, customers: list, cleaning_staff) -> None:
+    def movie_session(
+        self, 
+        movie_name: str, 
+        customers: list, 
+        cleaning_staff:Any
+    ) -> None:
         print(f'"{movie_name}" started in hall number {self.hall_number}.')
         for customer in customers:
             customer.watch_movie(movie_name)
