4cba240606fa01b26b4ee3b939b7c67447bd45e4
Volodymyr Stashenko <volodyasilver@gmail.com>
2026-03-24 22:10:41 +0200

    Refactor cinema_visit function parameters for clarity

--- app/main.py
+++ app/main.py
@@ -4,19 +4,21 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
-    # 1. Створюємо екземпляри клієнтів та продаємо їм їжу через статичний метод бару
+def cinema_visit(
+    customers: list,
+    hall_number: int,
+    cleaner: str,
+    movie: str
+) -> None:
     customer_instances = []
     for person_data in customers:
         customer = Customer(person_data["name"], person_data["food"])
         CinemaBar.sell_product(customer.food, customer)
         customer_instances.append(customer)
 
-    # 2. Створюємо персонал (прибиральника) та зал
     cleaner_instance = Cleaner(cleaner)
     hall_instance = CinemaHall(hall_number)
 
-    # 3. Запускаємо кіносеанс (він сам запустить перегляд та прибирання)
     hall_instance.movie_session(
         movie_name=movie,
         customers=customer_instances,
